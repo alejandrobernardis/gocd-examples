10b75879bf88669887ef3322b5cdfaeac24cab90
Alejandro M. BERNARDIS <alejandro.bernardis@gmail.com>
2021-02-25 11:02:19 -0300

    ADD // Fixes

--- step_2.sh
+++ step_2.sh
@@ -3,4 +3,4 @@
 echo "GOCD / Step 2"
 echo "docker run --rm --name px1 -v $PWD:/workspace python:3.9-alpine python /workspace/step_2.py"
 
-docker run --rm --name px1 -v $PWD:/workspace python:3.9-alpine python /workspace/step_2.py
+docker run --name px1 -v $PWD:/workspace python:3.9-alpine python /workspace/step_2.py
